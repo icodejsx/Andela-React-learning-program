39da95bb1b6f4d005a0cfb3927ce65d2cf7a0c4d
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-08-01 15:00:26 +0100

    addding json
    just adding the json

--- myapp/src/app/components/Header.jsx
+++ myapp/src/app/components/Header.jsx
@@ -7,6 +7,9 @@ const Header = () => {
             <div>
                 <img src='/logoo.png' className='w-20' />
             </div>
+            <h1>
+                Providing House All Over the World
+            </h1>
 
         </header>
     );
